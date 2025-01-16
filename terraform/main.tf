34dce7f473424dd40c277552dfbce63b0eb30fdc
joshatre <joshatre@gmail.com>
2025-01-16 12:03:54 -0800

    Added cluster certificate to kubernetes provider configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,7 @@ provider "google" {
 provider "kubernetes" {
   host = "https://${google_container_cluster.primary.endpoint}"
   token = data.google_client_config.current.access_token
-#  config_context_cluster = "gke_${var.project}_${var.zone}_${google_container_cluster.primary.name}"
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
 }
 
 data "google_client_config" "current" {}
